47a08440d1f33ebbae85573b1a696c5d7dc3bf20
Darya <72987661+DRomanova-A@users.noreply.github.com>
2021-10-24 13:22:36 +0300

    Add files via upload

---
+++ основы программирования python (ВШЭ)/solutions/week-2/сред знач пос-ти.py
@@ -0,0 +1,9 @@
+
+n = int(input())
+sum = n
+count = 0
+while n != 0:
+    n = int(input())
+    sum += n
+    count += 1
+print(sum / count)
---
+++ основы программирования python (ВШЭ)/solutions/week-2/сумма последовательности.py
@@ -0,0 +1,7 @@
+
+n = int(input())
+sum = n
+while n != 0:
+    n = int(input())
+    sum += n
+print(sum)
